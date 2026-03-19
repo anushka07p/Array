300f5b78d69df221a7ab7857d45d89c3fa935b2d
anushka07p <kp1468698@gmail.com>
2026-03-19 23:52:42 +0530

    Implement Trapping Rain Water algorithm

---
+++ TrappingRainwater.java
@@ -0,0 +1,29 @@
+public class TrappingRainWater{
+    public static int trappingwater(int height[]){
+        int n = height.length;
+        //left max
+        int leftmax[]=new int[n];
+        leftmax[0]=height[0];
+        for(int i =1;i<n;i++){
+            leftmax[i]=Math.max(height[i],leftmax[i-1]);
+        }
+        //rightmax
+        int rightmax[]=new int[n];
+        rightmax[n-1]=height[n-1];
+        for(int i=n-2;i>=0;i--){
+            rightmax[i]=Math.max(height[i],rightmax[i+1]);
+        }
+        //loop
+        int trappedwater=0;
+        for(int i =0;i<n;i++){
+            int waterlevel=Math.min(leftmax[i],rightmax[i]);
+        //trappedwater
+         trappedwater += waterlevel-height[i];
+        }
+        return trappedwater;
+    }
+    public static void main(String[] args){
+        int height[]={4,2,0,3,2,5};
+        System.out.println(trappingwater(height));
+    }
+}
